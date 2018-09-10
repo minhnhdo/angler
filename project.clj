af5692e3d177e5ef82dde246eb55c6496c019fd3
Do Nhat Minh <mrordinaire@gmail.com>
2018-09-09 19:20:13 -0700

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject angler "0.1.0-SNAPSHOT"
+  :description "Angler - a probabilistic programming language"
+  :url "https://github.com/mrordinaire/angler"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot angler.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
