912c43fadd8f3f8876d1de6ddc7e572d017960c4
Do Nhat Minh <mrordinaire@gmail.com>
2018-09-09 22:22:44 -0700

    handle no command line argument case

--- src/angler/core.clj
+++ src/angler/core.clj
@@ -12,20 +12,21 @@
   [option-summary]
   (->> ["Angler - a probabilistic programming language"
         ""
-        "Usage: program-name [options] input-files|-"
+        "Usage: program-name [options] [input-files|-]..."
         ""
         "Options:"
         option-summary]
        (string/join \newline)))
 
 (defn process-options
-  [{:keys [options summary errors] :as parsed-options}]
+  [{:keys [options summary errors arguments] :as parsed-options}]
   (cond
     (:help options) (do (println (usage summary))
                         (System/exit 1))
     errors (do (println (str "Unable to parse command line arguments:\n\n"
                              (string/join \newline errors)))
                (System/exit 1))
+    (nil? (seq arguments)) (assoc parsed-options :arguments ["-"])
     :else parsed-options))
 
 (defn -main
