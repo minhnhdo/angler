b13077b331caa598e21c695f03d7a8007f26ae5c
Do Nhat Minh <mrordinaire@gmail.com>
2018-09-09 21:54:28 -0700

    can read forms

--- src/angler/core.clj
+++ src/angler/core.clj
@@ -1,7 +1,41 @@
 (ns angler.core
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
+(def cli-options
+  [["-h" "--help"]])
+
+(defn usage
+  [option-summary]
+  (->> ["Angler - a probabilistic programming language"
+        ""
+        "Usage: program-name [options] input-files|-"
+        ""
+        "Options:"
+        option-summary]
+       (string/join \newline)))
+
+(defn process-options
+  [{:keys [options summary errors] :as parsed-options}]
+  (cond
+    (:help options) (do (println (usage summary))
+                        (System/exit 1))
+    errors (do (println (str "Unable to parse command line arguments:\n\n"
+                             (string/join \newline errors)))
+               (System/exit 1))
+    :else parsed-options))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments]} (process-options (parse-opts args cli-options))]
+    (doseq [f arguments]
+      (with-open [r (java.io.PushbackReader.  (if (= "-" f) *in* (io/reader f)))]
+        (println
+          (loop [exps []]
+            (let [e (edn/read {:readers {}, :eof ::done} r)]
+              (if (= ::done e)
+                exps
+                (recur (conj exps e))))))))))
