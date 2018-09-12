55617fad1c7d0c0c1cc8c4d6971e9fe5a0dc458e
Do Nhat Minh <mrordinaire@gmail.com>
2018-09-12 00:00:17 -0700

    added example for foreach

---
+++ resources/examples/e7.clj
@@ -0,0 +1,9 @@
+(let [y-values [2.1 3.9 5.3 7.7 10.2]
+      slope (sample (normal 0.0 10.0))
+      intercept (sample (normal 0.0 10.0))]
+  (foreach 5
+    [x (range 1 6)
+     y y-values]
+    (let [fx (+ (* slope x) intercept)]
+      (observe (normal fx 1.0) y)))
+  [slope intercept])
