56e522ba06f34e038a6bb028e6b4c7b75a8f86c8
Do Nhat Minh <mrordinaire@gmail.com>
2018-09-12 11:52:18 -0700

    added Graph

---
+++ src/angler/types.clj
@@ -0,0 +1,28 @@
+(ns angler.types
+  (:require clojure.set))
+
+(defrecord Graph
+  [^clojure.lang.IPersistentSet V
+   ^clojure.lang.IPersistentSet A
+   ^clojure.lang.IPersistentMap P
+   ^clojure.lang.IPersistentMap Y])
+
+(defn empty-graph
+  []
+  (Graph. #{} #{} {} {}))
+
+(defn join-graph
+  ^Graph
+
+  ([] (empty-graph))
+
+  ([^Graph graph] graph)
+
+  ([^Graph graph1 ^Graph graph2]
+   (Graph. (clojure.set/union (:V graph1) (:V graph2))
+           (clojure.set/union (:A graph1) (:A graph2))
+           (merge (:P graph1) (:P graph2))
+           (merge (:Y graph1) (:Y graph2))))
+
+  ([^Graph graph1 ^Graph graph2 & graphs]
+   (reduce join-graph (join-graph graph1 graph2) graphs)))
