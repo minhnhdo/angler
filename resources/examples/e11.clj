8c1441ce754b4151ab2f6b7983498a34ac00f6c4
Do Nhat Minh <mrordinaire@gmail.com>
2018-09-25 01:15:59 -0700

    test more

--- resources/examples/e11.clj
+++ resources/examples/e11.clj
@@ -2,8 +2,8 @@
       slope (sample (normal 0.0 10.0))
       intercept (sample (normal 0.0 10.0))]
   (foreach 5
-    [x (range 1 6)
-     y y-values]
-    (let [fx (+ (* slope x) intercept)]
-      (observe (normal fx 1.0) y)))
+           [x (range 1 6)
+            y y-values]
+           (let [fx (+ (* slope x) intercept)]
+             (observe (normal fx 1.0) y)))
   [slope intercept])
